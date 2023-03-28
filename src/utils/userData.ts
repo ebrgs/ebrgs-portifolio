9f7bbf9999be47b6f596f0e5c0b2d8380d652764
Elias <e.borges289@gmail.com>
2023-03-28 18:20:23 -0300

    atualizando dados

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
   nameUser: "Elias Borges",
-  githubUser: "Ebrgs",
+  githubUser: "ebrgs",
   linkedinUser: "eliasborgess",
   whatsappNumber: "+5586994175999",
   emailUser: "e.borges289@gmail.com",
