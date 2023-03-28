54739ecb06b053d8824fc1682e9d4c94477ad913
Elias <e.borges289@gmail.com>
2023-03-28 18:12:31 -0300

    Atualizando dados

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Elias Borges",
+  githubUser: "Ebrgs",
+  linkedinUser: "eliasborgess",
+  whatsappNumber: "+5586994175999",
+  emailUser: "e.borges289@gmail.com",
+  instagramUser: "_elias.borges",
   facebookUser: "",
 };
